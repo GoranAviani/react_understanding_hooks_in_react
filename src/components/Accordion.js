aa337351835e342e7f9a25d659de2f5d3d8dd49f
GoranAviani <goranaviani@gmail.com>
2022-04-19 20:46:47 +0200

    react_understanding_hooks_in_react

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-export default ({items}) => {
+const Accordion = ({items}) => {
 
     return (
         <div>
@@ -9,3 +9,5 @@ export default ({items}) => {
         </div>
     )
 }
+
+export default Accordion;
