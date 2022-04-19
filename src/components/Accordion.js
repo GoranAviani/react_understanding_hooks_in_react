5c5b27458817223e29803154b0a9b880da1f3e58
GoranAviani <goranaviani@gmail.com>
2022-04-19 20:48:27 +0200

    react_understanding_hooks_in_react

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,6 +2,20 @@ import React from 'react';
 
 
 const Accordion = ({items}) => {
+    const renderedItems = items.map(item => {
+        return(
+            <div className='title active'>
+                <i className='dropdown icon'></i>
+                {item.title}
+            </div>
+            <div className='content active'>
+                <p>
+                    {item.content}
+                </p>
+
+            </div>
+        )
+    })
 
     return (
         <div>
