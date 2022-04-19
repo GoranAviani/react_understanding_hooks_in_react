25ade849de421599d1c7344c75e6db7e384c0f66
GoranAviani <goranaviani@gmail.com>
2022-04-19 20:45:35 +0200

    react_understanding_hooks_in_react

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,23 +1,14 @@
 import React from 'react';
 
-const items = [
-    {title: 'What is react?',
-    content: 'React is a frond end js framework'},
-    {title: 'Why use react?',
-    content: 'React is one of favourite JS library among engineers'},
-    {
-        title: 'How to you use React?',
-        content: 'You use react creating components'
-    }
-]
 
 
 
-export default () => {
+
+export default ({items}) => {
 
     return (
         <div>
-            Accordion 1
+            {items.length}
         </div>
     )
 }
