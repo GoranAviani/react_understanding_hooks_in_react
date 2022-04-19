248c8cb975bd0c46b65c050454b6c762467e5e12
GoranAviani <goranaviani@gmail.com>
2022-04-19 20:28:45 +0200

    react_understanding_hooks_in_react

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App'
 
-ReactDOM.render(<App></App>, document.querySelector('#root'))
+ReactDOM.render(<App/>, document.querySelector('#root'))
