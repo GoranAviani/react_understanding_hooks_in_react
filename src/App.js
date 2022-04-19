2333ee0d8548f714dc5691653b62590c2583b4ea
GoranAviani <goranaviani@gmail.com>
2022-04-19 20:44:57 +0200

    react_understanding_hooks_in_react

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import Accordion from "./components/Accordion";
 
 class App extends React.Component {
 
-    render() {
+export default () => {
+
+
         return (
             <div>
                 app
                 <Accordion/>
             </div>
         )
-    }
+
 }
 
-export default App;
