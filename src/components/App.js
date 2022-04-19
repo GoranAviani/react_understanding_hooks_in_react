4b6095d7ca6627dccbaf4decdc0fc7e06b495029
GoranAviani <goranaviani@gmail.com>
2022-04-19 20:35:35 +0200

    react_understanding_hooks_in_react

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import Accordion from "./Accordion";
+
 class App extends React.Component {
 
     render() {
         return (
             <div>
                 app
+                <Accordion/>
             </div>
         )
     }
