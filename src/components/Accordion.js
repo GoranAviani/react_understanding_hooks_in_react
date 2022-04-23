4c8ba56a04abd4eafb75ad7dd649b0bde914d8c6
GoranAviani <goranaviani@gmail.com>
2022-04-23 13:20:45 +0200

    react_understanding_hooks_in_react

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -11,9 +11,11 @@ const Accordion = ({items}) => {
     };
 
     const renderedItems = items.map((item, index) => {
+        const active = index === activeIndex ? 'active': '';
+
         return (
             <React.Fragment key={item.title}>
-                <div className='title active'
+                <div className={`title ${active}`}
                      onClick={() => onTitleClick(index)}>
                     <i className='dropdown icon'></i>
                     {item.title}
