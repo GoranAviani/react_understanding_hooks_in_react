160cdb34058a80071a3d3e8b2e68abbb1d94e483
GoranAviani <goranaviani@gmail.com>
2022-04-26 17:54:10 +0200

    react_understanding_hooks_in_react

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React , {useState} from "react";
 
 const Search = () => {
-    return <div>search</div>
+    const [term, setTerm] = useState('')
+
+
+    return (
+        <div className="ui form">
+            <div className="field">
+                <label> enter search term</label>
+                <input className="input"
+                value = {term}
+                onChange = {inputHandler}/>
+            </div>
+        </div>
+    )
 }
 
 export default Search;
