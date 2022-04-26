262d4848461c1a929a89f44d75b905c99c65a208
GoranAviani <goranaviani@gmail.com>
2022-04-26 17:54:18 +0200

    react_understanding_hooks_in_react

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,6 +3,9 @@ import React , {useState} from "react";
 const Search = () => {
     const [term, setTerm] = useState('')
 
+    const inputHandler = (e) => {
+        setTerm(e.target.value)
+    }
 
     return (
         <div className="ui form">
