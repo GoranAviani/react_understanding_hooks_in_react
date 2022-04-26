e045cbdc87887753f5df63c9353a01d70b2005cf
GoranAviani <goranaviani@gmail.com>
2022-04-26 18:04:00 +0200

    react_understanding_hooks_in_react

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,9 @@
-import React , {useState} from "react";
+import React , {useState, useEffect} from "react";
 
 const Search = () => {
     const [term, setTerm] = useState('')
+
+    useEffect(()=>{console.log({term})}, [term])
 
     const inputHandler = (e) => {
         setTerm(e.target.value)
